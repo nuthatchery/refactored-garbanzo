32b29789383f874571562f8de1458d0ac16710bf
anna <anna@10.111.29.124>
2017-07-17 18:43:44 +0200

    flattened list, added flat link
    no test for links though, unsure how to test atm

--- src/rascalsol/ModelTree.java
+++ src/rascalsol/ModelTree.java
@@ -5,22 +5,42 @@ import java.util.HashMap;
 import java.util.List;
 
 public class ModelTree {
-	//	private ArrayList<IIdentity> b = new ArrayList<>();
-	private IIdentity[][] B;
-	private HashMap<IIdentity, Integer> bindex = new HashMap<>();
-	//	private ArrayList<IIdentity> l = new ArrayList<>();
-	private IIdentity[][] L;
-	private HashMap<IIdentity, Integer> lindex = new HashMap<>();
-	private IIdentity root;
+	/**
+	 * flat edges: such that for any 0<=i<b.size() we have b(i) = parent, b(i+1) = edge, b(i+2) = child
+	 */
+	private ArrayList<IIdentity> b;
 
-	private List<IIdentity> orderednodes = new ArrayList<>(); 
+	/**
+	 * map: Node -> first entry of children in B such that 
+	 * bindex.get(n) = i where i is the smallest index 0<=i<b.size such that (i+1) % 3 = 1 and b(i)=n
+	 */
+	private HashMap<IIdentity, Integer> bindex = new HashMap<>();
+
+	private ArrayList<IIdentity> l;
+	/**
+	 * map: Node -> first entry of links in L such that 
+	 * lindex.get(n) = i where 0<=i<L.length such that L[i][0] points to n, and there is no j<i such that L[j][0] points to n
+	 */
+	private HashMap<IIdentity, Integer> lindex = new HashMap<>();
+	/**
+	 * the root of this model (no good reason to keep this around)
+	 */
+	private IIdentity root;
+	/**
+	 * the full set of Ns of this model, ordered by a preorder tree traversal 
+	 */
+	private List<IIdentity> orderednodes = new ArrayList<>();
+
 
 	public ModelTree(IIdentity root){
-		B = new IIdentity[0][3];
-		L = new IIdentity[0][3];
+
+		b = new ArrayList<IIdentity>();
+		l = new ArrayList<IIdentity>();
+
+		bindex.put(root, b.size());
+		lindex.put(root, l.size());
+
 		orderednodes.add(root);
-		bindex.put(root, B.length);
-		lindex.put(root, L.length);
 		this.root = root;
 		datainvariant();
 	}
@@ -35,92 +55,137 @@ public class ModelTree {
 	}
 
 	public ModelTree addChild(IIdentity child, IIdentity edge, IIdentity parent){
+		int next = orderednodes.indexOf(parent);
+		if (next<0) throw new IllegalArgumentException("parent must be a node in this model: " + parent);
+		next = next+1;
+
+		//		System.out.println("Adding " + child);
 		ModelTree m = new ModelTree(this);
 		//		ModelTree m = this; //mutable
 
-		m.B = new IIdentity[B.length+1][3];
+		cloneLinksTo(m);
+
+		//System.arraycopy(B, 0, dest, destPos, length);
+		//Collections.addAll(b, [])
+		//List.addAll(b)
+
+		int parentIndexB = bindex.get(parent);
+		Integer toIndex = bindex.get(next);
+		if(toIndex == null)
+			toIndex = b.size();
+		m.b = new ArrayList<IIdentity>(b.subList(0, toIndex));
+		m.b.add(parent);
+		m.b.add(edge);
+		m.b.add(child);
+		
+		b.subList(toIndex, b.size()).forEach(elem->m.b.add(elem));
+
 		m.bindex = new HashMap<IIdentity, Integer>();
-
-		m.L = L.clone();
-		m.lindex = (HashMap<IIdentity, Integer>) lindex.clone();
-
-		if(bindex.get(parent)!=null){ //else what to do 
-			int parentIndexB = bindex.get(parent);
-			
-			bindex.forEach((id, bi)->{
-				if(bi<=parentIndexB) m.bindex.put(id, bi); 
-				else m.bindex.put(id,  bi+1);
-			}
-					);
-			m.bindex.put(child, B.length);
-			
-			int i=0;
-			//copy all edges before parent's children
-			for(; i<parentIndexB; i++){
-				m.B[i][0] = B[i][0];
-				m.B[i][1] = B[i][1];
-				m.B[i][2] = B[i][2];
-			}
-			//copy parent's children
-			for(; i<B.length && B[i][0].equals(parent); i++){
-				m.B[i][0] = B[i][0];
-				m.B[i][1] = B[i][1];
-				m.B[i][2] = B[i][2];
-			}
-				
-			m.B[i][0] = parent;
-			m.B[i][1] = edge;
-			m.B[i][2] = child;
-			
-			for(i=i+1; i<B.length; i++){
-				m.B[i+1][0] = B[i][0];
-				m.B[i+1][1] = B[i][1];
-				m.B[i+1][2] = B[i][2];
-				print(m.B);
-			}
+		bindex.forEach((id, bi)->{
+			if(bi<parentIndexB) m.bindex.put(id, bi); 
+			else m.bindex.put(id,  bi+3);
 		}
+				);
+		m.bindex.put(parent, parentIndexB);
+		m.bindex.put(child, m.b.size());
+		//			m.bindex.forEach((a, b) -> System.out.println(a + " -> " + b));
 
 		m.orderednodes.clear(); 
 		m.getAll().forEach(elem-> m.orderednodes.add(elem));
-		datainvariant();
+		datainvariant(); //TODO not really needed
+		m.datainvariant();
 		return m;
 	}
 
-	private void print(IIdentity[][] b2) {
-		System.out.println("printing arr");
-		for(int i=0; i<b2.length; i++){
-			System.out.println(b2[i][0] + ", " + b2[i][1] + ", " +b2[i][2]);
-		}
+	private void cloneLinksTo(ModelTree m) {
+		m.l = (ArrayList<IIdentity>) l.clone();
+		m.lindex = (HashMap<IIdentity, Integer>) lindex.clone();
 	}
 
+	public ModelTree addLink(IIdentity from, IIdentity link, IIdentity to){
+		int next = orderednodes.indexOf(from);
+		if (next<0) throw new IllegalArgumentException("from must be a node in this model: " + from);
+		next = next+1;
+
+		//		System.out.println("Adding " + child);
+		ModelTree m = new ModelTree(this);
+		//		ModelTree m = this; //mutable
+
+		cloneEdgesTo(m);
+
+		//System.arraycopy(B, 0, dest, destPos, length);
+		//Collections.addAll(b, [])
+		//List.addAll(b)
+
+		int parentIndexB = lindex.get(from);
+		Integer toIndex = lindex.get(next);
+		if(toIndex == null)
+			toIndex = l.size();
+		m.b = new ArrayList<IIdentity>(l.subList(0, toIndex));
+		m.l.add(from);
+		m.l.add(link);
+		m.l.add(to);
+		
+		l.subList(toIndex, l.size()).forEach(elem->m.l.add(elem));
+
+		m.lindex = new HashMap<IIdentity, Integer>();
+		lindex.forEach((id, bi)->{
+			if(bi<parentIndexB) m.lindex.put(id, bi); 
+			else m.lindex.put(id,  bi+3);
+		}
+				);
+		m.lindex.put(from, parentIndexB);
+		m.lindex.put(to, m.l.size());
+		//			m.lindex.forEach((a, b) -> System.out.println(a + " -> " + b));
+
+		m.orderednodes.clear(); 
+		m.getAll().forEach(elem-> m.orderednodes.add(elem));
+		datainvariant(); //TODO not really needed
+		m.datainvariant();
+		return m;
+	}
+
+	private void cloneEdgesTo(ModelTree m) {
+		m.b = (ArrayList<IIdentity>) b.clone();
+		m.bindex = (HashMap<IIdentity, Integer>) bindex.clone();
+	}
+	
 	/**
 	 * @return an iterable of the nodes in prefix traversal
 	 */
 	public Iterable<IIdentity> getAll(){
 		List<IIdentity> ls = new ArrayList<>();
 
-		class NodeAdder{
+		class LocalListNodeAdder{
 			void addNode(IIdentity n){
 				ls.add(n);
-				if(bindex.get(n)==null||bindex.get(n)>=B.length)return;
-				for(int i=bindex.get(n); i<B.length && B[i] != null && B[i][0].equals(n); i++){
-					addNode(B[i][2]);
+				if(bindex.get(n)==null||bindex.get(n)>=b.size())return;
+				for(int i=bindex.get(n); i<b.size() && b.get(i) != null && b.get(i).equals(n); i+=3){
+					addNode(b.get(i+2));
 				}
 			}
 		};
 
-		new NodeAdder().addNode(root);
+		new LocalListNodeAdder().addNode(root);
+		//		System.out.println("printing in getAll");
+		//		ls.forEach(l->System.out.print(l));
+		//		System.out.println();
 		return ls;
 	}
 	
-	public void datainvariant(){
+	private void datainvariant(){
 		orderednodes.forEach(item -> {assert bindex.containsKey(item) : "All nodes should be found in bindex: " + item;});
+
+		bindex.forEach((item, i) -> {assert orderednodes.contains(item): "all parent nodes of model should be recorded in the model's list of nodes: " + item;});
+
 		orderednodes.forEach(item -> {assert bindex.get(item)!=null: "No nodes should have index null: " + item;});
+
 		for(int i=0; i<orderednodes.size(); i++){
 			IIdentity n = orderednodes.get(i);
-			for(int j = i+1; j<orderednodes.size(); j++){
-				if(bindex.get(orderednodes.get(j))!=B.length)
-					assert bindex.get(n) <= bindex.get(orderednodes.get(j)) : "Ordered nodes should have increasing branch-index";
+			if(bindex.get(n)!=b.size()){
+				for(int j = i+3; j<orderednodes.size(); j+=3){
+					assert bindex.get(n) <= bindex.get(orderednodes.get(j)) : "Ordered nodes should have increasing branch-index: " + n + "->" + bindex.get(n) + " <= " + orderednodes.get(j) + "->" + bindex.get(orderednodes.get(j));
+				}
 			}
 		}
 	}
--- src/rascalsol/TestModelTree.java
+++ src/rascalsol/TestModelTree.java
@@ -1,9 +1,19 @@
 package rascalsol;
 
+import org.junit.Test; 
 
-public class Test {
+public class TestModelTree {
 
-	public static void main(String[] args){
+	@Test
+	public void testSingleton(){
+		IIdentity en = new Identity("1");
+		ModelTree m = new ModelTree(en);
+		m.getAll().forEach(elem->System.out.print(elem + " "));
+		System.out.println();
+	}
+	
+	@Test
+	public void testEdgeVerySmallTree(){
 		IIdentity pluss = new Identity("+");
 		IIdentity en = new Identity("1");
 		IIdentity to = new Identity("2");
@@ -11,6 +21,29 @@ public class Test {
 		m = m.addChild(en, new Identity("left"), pluss);
 		m = m.addChild(to, new Identity("right"), pluss);
 		m.getAll().forEach(elem->System.out.print(elem + " "));
+		System.out.println();
 	}
-
+	
+	@Test
+	public void testEdgeSmallRightHeavyTree(){
+		IIdentity pluss = new Identity("+");
+		IIdentity minus = new Identity("-");
+		IIdentity en = new Identity("1");
+		IIdentity to = new Identity("2");
+		IIdentity tre = new Identity("3");
+		ModelTree m = new ModelTree(pluss);
+		m = m.addChild(tre, new Identity("left"), pluss);	//+ 3
+//		m.getAll().forEach(elem->System.out.print(elem + " "));
+//		System.out.println();
+		m = m.addChild(minus, new Identity("right"), pluss); // + 3 -
+//		m.getAll().forEach(elem->System.out.print(elem + " "));
+//		System.out.println();
+		m = m.addChild(to, new Identity("left"), minus);	// + 3 - 2 
+//		m.getAll().forEach(elem->System.out.print(elem + " "));
+//		System.out.println();
+		m = m.addChild(en, new Identity("right"), minus); // + 3 - 2 1 
+		m.getAll().forEach(elem->System.out.print(elem + " "));
+	}
+	
+	
 }
