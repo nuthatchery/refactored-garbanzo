855f093fb5c7a0730cf1179e04253fcecefaee11
Anna Maria Eilertsen <anna@Annas-MacBook-Pro-3.local>
2017-09-05 15:09:00 +0200

    added test for exploring interface

---
+++ src/adevpck/TestModelLang.java
@@ -0,0 +1,58 @@
+package adevpck;
+
+import org.junit.Test;
+
+/**
+ * If we have a language 
+ * 
+ * S -> INT + INT 
+ * INT -> INT + INT 
+ * INT -> num 
+ *  
+ * @author anna
+ *
+ */
+public class TestModelLang {
+
+	@Test
+	public void testbinoplang(){
+		/*
+		 * Meta: 
+		 * binOp, edge, leftArg
+		 * binOp, edge, rightArg
+		 */
+		IIdentity larg = new Identity("leftArg");
+		IIdentity rarg = new Identity("rightArg");
+		ModelTree binopmeta = new ModelTree().beginTransaction();
+		binopmeta.makeRoot(new Identity("binOp"))
+		.addChildToRoot(new Identity("edge"), larg)
+		.addChildToRoot(new Identity("edge"), rarg)
+		.commitTransaction();
+
+		/*
+		 * tree: 
+		 * pluss, edge, arg
+		 * pluss, edge, arg
+		 * 
+		 * links:
+		 * plus, binop
+		 * edge, leftArg
+		 * edge, rightArg
+		 */	
+		IIdentity plus = new Identity("plus");
+		IIdentity arg1 = new Identity("arg");
+		IIdentity arg2 = new Identity("arg");
+		IIdentity edge = new Identity("edge");
+		ModelTree binoplang = new ModelTree().beginTransaction();
+		binoplang.makeRoot(plus)
+		.addLink(plus, binopmeta.getRoot())
+		.addChildToRoot(edge, arg1)
+		.addLink(edge, larg)
+		.addChildToRoot(edge, arg2)
+		.addLink(edge, rarg)
+		.commitTransaction();
+
+		
+		System.out.println(binoplang);
+	}	
+}
